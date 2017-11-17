997e8eb20cde530b116bb553fe3177969936c7a5
Stoyan Stratev <stoyan.stratev@telerik.com>
2017-11-17 14:47:08 +0200

    Add Vanilla TS demo app

---
+++ demo/README.md
@@ -0,0 +1,8 @@
+This NativeScript project demonstrates how the plugin works.
+If you want to test it out on an emulator or a device you can follow the instructions below:
+
+* `git clone https://github.com/NativeScript/NativeScript/push-plugin.git`
+* `cd push-plugin/demo` or `cd push-plugin/demo-angular`
+* `npm run build.plugin && npm install`
+* `tns run android` or `tns run ios` depending on the platform you want to test
+
---
+++ demo/app/main-view-model.ts
@@ -0,0 +1,91 @@
+import { Observable } from "data/observable";
+import * as pushPlugin from "nativescript-push-notifications";
+
+export class HelloWorldModel extends Observable {
+
+    private pushSettings = {
+        // Android settings
+        senderID: "<ENTER_YOUR_PROJECT_NUMBER>", // Android: Required setting with the sender/project number
+        notificationCallbackAndroid: (message: String, stringifiedData: String, fcmNotification: any) => {
+            const notificationBody = fcmNotification && fcmNotification.getBody();
+            this.updateMessage("Message received!\n" + notificationBody + "\n" + stringifiedData);
+        },
+
+        // iOS settings
+        badge: true, // Enable setting badge through Push Notification
+        sound: true, // Enable playing a sound
+        alert: true, // Enable creating a alert
+        notificationCallbackIOS: (message: String) => {
+            this.updateMessage("Message received!\n" + message);
+        }
+    };
+
+    private _counter: number;
+    private _message: string;
+
+    constructor() {
+        super();
+        this.message = "";
+        this.updateMessage("App started.");
+
+        let self = this;
+        this.onRegisterButtonTap();
+    }
+
+    get message(): string {
+        return this._message;
+    }
+
+    set message(value: string) {
+        if (this._message !== value) {
+            this._message = value;
+            this.notifyPropertyChange("message", value);
+        }
+    }
+
+    onCheckButtonTap() {
+        let self = this;
+        pushPlugin.areNotificationsEnabled((areEnabled: Boolean) => {
+            self.updateMessage("Are Notifications enabled: " + !!areEnabled);
+        });
+    }
+
+    onRegisterButtonTap() {
+        let self = this;
+        pushPlugin.register(this.pushSettings, (token: String) => {
+            self.updateMessage("Device registered. Access token: " + token);
+
+            if (pushPlugin.onMessageReceived) {
+                pushPlugin.onMessageReceived(this.pushSettings.notificationCallbackAndroid);
+            }
+
+            if (pushPlugin.registerUserNotificationSettings) {
+                pushPlugin.registerUserNotificationSettings(() => {
+                    self.updateMessage("Successfully registered for interactive push.");
+                }, (err) => {
+                    self.updateMessage("Error registering for interactive push: " + JSON.stringify(err));
+                });
+            }
+        }, (errorMessage: String) => {
+            self.updateMessage(JSON.stringify(errorMessage));
+        });
+    }
+
+    onUnregisterButtonTap() {
+        let self = this;
+        pushPlugin.unregister(
+            (successMessage: String) => {
+                self.updateMessage(successMessage);
+            },
+            (errorMessage: String) => {
+                self.updateMessage(JSON.stringify(errorMessage));
+            },
+            this.pushSettings
+        );
+    }
+
+    private updateMessage(text: String) {
+        this.message += text + "\n";
+    }
+
+}
