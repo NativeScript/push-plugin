597f9d279e30851b5e612580f2720d66a6b823f0
Josep Maria Viladegut <josep-maria.viladegut@gft.com>
2018-02-08 16:04:57 +0100

    Added hook to copy the "entitlements" files after prepare

---
+++ hooks/entitlements-after-prepare.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (logger, platformsData, projectData, hookArgs) {
+	var platform = hookArgs.platform.toLowerCase(),
+			appResourcesDirectoryPath = projectData.appResourcesDirectoryPath,
+			entitlementsFile = path.join(appResourcesDirectoryPath, "iOS", projectData.projectName + ".entitlements"),
+			projectRoot = path.join(projectData.platformsDir, "ios"),
+			project = path.join(projectRoot, projectData.projectName);
+
+	return new Promise(function (resolve, reject) {
+		if (platform === 'ios' && fs.existsSync(entitlementsFile)) {
+			var dest = path.join(project, projectData.projectName + ".entitlements");
+			fs.createReadStream(entitlementsFile).pipe(fs.createWriteStream(dest));
+		}
+		resolve();
+	});
+};
