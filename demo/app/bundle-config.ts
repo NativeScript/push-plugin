8b34f49f5a87fdefdfc76ed8422277ae69b1b630
Dimitar Todorov <Dimitar.Todorov@progress.com>
2018-06-01 15:51:55 +0300

    Merge next into master

--- demo/app/bundle-config.ts
+++
@@ -1,9 +0,0 @@
-if ((<any>global).TNS_WEBPACK) {
-    // registers tns-core-modules UI framework modules
-    require("bundle-entry-points");
-
-    // register application modules
-    global.registerModule("main-page", () => require("./main-page"));
-
-    global.registerModule("nativescript-push-notifications", () => require("nativescript-push-notifications"));
-}
